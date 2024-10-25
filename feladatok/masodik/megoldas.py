322a4013ab1e1aecbb2d4005f9a3a2c1eaaae931
orsiczako <146200985+orsiczako@users.noreply.github.com>
2024-10-25 20:20:33 +0200

    Update megoldas.py

--- feladatok/masodik/megoldas.py
+++ feladatok/masodik/megoldas.py
@@ -1,31 +1,30 @@
-
 def jegyvasarlas():
-    # Ez a függvény a jegyvásárlási folyamatot szimulálja.
+    # Kezdeti érték a valasz változónak, hogy elindulhasson a ciklus.
+    valasz = ""
+    
+    while valasz != "kilépés":  
+        # A ciklus addig fut, amíg a felhasználó nem írja be, hogy "kilépés".
+        valasz = input('Milyen jegyet szeretnél venni? (kilépés - kilépés)\n')
+        # Megkérdezzük, milyen jegyet szeretne venni a felhasználó.
 
-    while True:  # Végtelen ciklus, ami addig tart, amíg a felhasználó a "kilépés" szót nem adja meg.
-        jegytipus = input('Milyen jegyet szeretnél venni? (kilépés - kilépés)\n')
-        # Kérjük a felhasználót, hogy válasszon a jegytípusok közül.
-
-        if jegytipus == 'kilépés':
-            # Ha a felhasználó a "kilépés" szót írja be, a program véget ér.
-            print('Kilépés a programból...')
-            break  # Kilépünk a ciklusból, és befejezzük a programot.
-
-        elif jegytipus == 'gyerek':
-            # Ha a felhasználó "gyerek" jegyet választ, kiírjuk a gyerekjegy árát.
+        if valasz == 'gyerek':  
+            # Ha "gyerek"-jegyet választ, kiírjuk a gyerekjegy árát.
             print('A gyerekjegy ára: 1000 Ft.')
 
-        elif jegytipus == 'felnőtt':
-            # Ha a felhasználó "felnőtt" jegyet választ, kiírjuk a felnőttjegy árát.
+        elif valasz == 'felnőtt':  
+            # Ha "felnőtt"-jegyet választ, kiírjuk a felnőttjegy árát.
             print('A felnőttjegy ára: 1500 Ft.')
 
-        elif jegytipus == 'nyugdíjas':
-            # Ha a felhasználó "nyugdíjas" jegyet választ, kiírjuk a nyugdíjas jegy árát.
+        elif valasz == 'nyugdíjas':  
+            # Ha "nyugdíjas"-jegyet választ, kiírjuk a nyugdíjas jegy árát.
             print('A nyugdíjas jegy ára: 1200 Ft.')
 
-        else:
-            # Ha a felhasználó érvénytelen jegytípust választ, tájékoztatjuk, hogy próbálja újra.
+        elif valasz != "kilépés":  
+            # Ha nem érvényes jegytípust ír be, hibaüzenet jelenik meg.
             print('Érvénytelen választás, kérlek válassz egy érvényes jegytípust!')
             
+    print('Kilépés a programból...')
+    # A ciklus véget ér, ezért megjelenik a kilépési üzenet.
+    
 # Jegyvásárlási folyamat indítása
 jegyvasarlas()
