572bab05cec9543ab1cf1efa248b46bfc41b850b
orsiczako <146200985+orsiczako@users.noreply.github.com>
2024-10-25 20:21:02 +0200

    Update megoldas.py

--- feladatok/harmadik/megoldas.py
+++ feladatok/harmadik/megoldas.py
@@ -1,17 +1,30 @@
 def sportklub():
-    # Ez a függvény a sportklub programját szimulálja.
+    # A valasz változó kezdetben üres, hogy elindulhasson a ciklus.
+    valasz = ""
+    
+    while valasz != "kilépés":  
+        # A ciklus addig fut, amíg a felhasználó nem írja be, hogy "kilépés".
+        valasz = input('Mit szeretnél csinálni? (kilépés - kilépés)\n')
+        # Megkérdezzük, hogy mit szeretne csinálni a sportklubban.
 
-    while True:  # Végtelen ciklus, ami addig fut, amíg a felhasználó be nem írja a "kilépés" szót.
-        valasztas = input('Mit szeretnél csinálni? (kilépés - kilépés)\n')
-        # Kérjük a felhasználót, hogy válasszon az edzés, verseny vagy konditerem közül.
-
-        if valasztas == 'kilépés':
-            # Ha a felhasználó a "kilépés" szót írja be, a program kilép.
-            print('Kilépés a programból...')
-            break  # Megszakítjuk a ciklust és a program véget ér.
-
-        elif valasztas == 'edzés':
-            # Ha a felhasználó "edzés"-t választ, megjelenik az üzenet az edzésről.
+        if valasz == 'edzés':  
+            # Ha "edzés"-t választ, kiírjuk az edzéssel kapcsolatos információkat.
             print('Az edzés 16:00-kor kezdődik a sportpályán.')
 
-        elif valasztas == 'verseny
+        elif valasz == 'verseny':  
+            # Ha "verseny"-t választ, kiírjuk a verseny időpontját és helyszínét.
+            print('A verseny 14:00-kor kezdődik a stadionban.')
+
+        elif valasz == 'konditerem':  
+            # Ha "konditerem"-et választ, kiírjuk az erre vonatkozó információkat.
+            print('A konditerem egész nap nyitva van.')
+
+        elif valasz != "kilépés":  
+            # Ha nem érvényes választ ad, hibaüzenet jelenik meg.
+            print('Érvénytelen választás, kérlek válassz egy érvényes lehetőséget!')
+            
+    print('Kilépés a programból...')
+    # A ciklus véget ért, ezért megjelenik a kilépési üzenet.
+    
+# Sportklub program indítása
+sportklub()
